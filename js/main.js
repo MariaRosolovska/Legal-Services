ccb88210519d2e2e9d5530c9acdb53c2a1ae3715
Maria Rosolovska <feofanmaria19@gmail.com>
2022-04-01 12:40:49 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+
+  AOS.init({
+    duration: 1600,
+});
+
+  $('.slider__inner, .news__slider-inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    infinite: false
+  });
+
+  $('select').styler();
+
+  $('.header__btn-menu').on('click', function(){
+    $('.menu ul').slideToggle();
+  });
+
+
+  AOS.refresh();
+});
